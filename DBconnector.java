a483c3d2b654ed596091c264bbced0568df13223
Unknown <scottwilder3@gmail.com>
2018-04-15 21:57:34 -0500

    Connection to DB

---
+++ DBconnector.java
@@ -0,0 +1,27 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author Scott Wilder
+ */
+public class MyDB {
+    Connection con;
+    public Connection getCon(){
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            con = DriverManager.getConnection("jdbc:mysql://localhost:3306/Website","root","");
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(MyDB.class.getName()).log(Level.SEVERE, null, ex);
+            System.out.println(ex);
+        } catch (SQLException ex) {
+            Logger.getLogger(MyDB.class.getName()).log(Level.SEVERE, null, ex);
+             System.out.println(ex);
+        }
+    return con;
+    }
+    
+}
