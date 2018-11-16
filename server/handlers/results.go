1595018201fbb339858af9f6ec4bf7cd326f3605
mwalto7 <dev.mwalto7@gmail.com>
2018-11-16 17:27:17 -0600

    Added route handlers.

---
+++ server/handlers/results.go
@@ -0,0 +1,181 @@
+package handlers
+
+import (
+	"encoding/json"
+	"net/http"
+	"strconv"
+
+	"github.com/gorilla/mux"
+	"github.com/mwalto7/osb-website/server/database"
+)
+
+// ListResults lists all results.
+func ListResults(db database.OSBDatabase) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		results, err := db.ListResults()
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		b, err := json.Marshal(results)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusOK)
+
+		if _, err := w.Write(b); err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+	}
+}
+
+// ListResultsCreatedBy returns all results created by the user with the given user ID.
+func ListResultsCreatedBy(db database.OSBDatabase) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		idStr, ok := mux.Vars(r)["id"]
+		if !ok {
+			http.Error(w, `router: no "id" key in router`, http.StatusInternalServerError)
+			return
+		}
+
+		userID, err := strconv.ParseInt(idStr, 10, 64)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		results, err := db.ListResultsCreatedBy(userID)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		b, err := json.Marshal(results)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusOK)
+
+		if _, err := w.Write(b); err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+	}
+}
+
+// GetResult returns the result row with the matching result id.
+func GetResult(db database.OSBDatabase) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		idStr, ok := mux.Vars(r)["id"]
+		if !ok {
+			http.Error(w, `router: no "id" key in router`, http.StatusInternalServerError)
+			return
+		}
+
+		resultID, err := strconv.ParseInt(idStr, 10, 64)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		result, err := db.GetResult(resultID)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		b, err := json.Marshal(result)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusOK)
+
+		if _, err := w.Write(b); err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+	}
+}
+
+// AddResult inserts a new result row.
+func AddResult(db database.OSBDatabase) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		if r.Method != http.MethodPost {
+			w.WriteHeader(http.StatusMethodNotAllowed)
+			return
+		}
+
+		username, password, ok := r.BasicAuth()
+		if !ok {
+			w.WriteHeader(http.StatusForbidden)
+			return
+		}
+
+		user, err := db.GetUserByCredentials(username, password)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusForbidden)
+			return
+		}
+
+		result := &database.Result{
+			UserID: user.ID,
+		}
+		if err := json.NewDecoder(r.Body).Decode(&result); err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		if err := db.AddResult(result); err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		w.WriteHeader(http.StatusOK)
+	}
+}
+
+// DeleteResult deletes the result row with the matching result id.
+func DeleteResult(db database.OSBDatabase) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		idStr, ok := mux.Vars(r)["id"]
+		if !ok {
+			http.Error(w, `router: no "id" key in router`, http.StatusInternalServerError)
+			return
+		}
+
+		resultID, err := strconv.ParseInt(idStr, 10, 64)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		if err := db.DeleteResult(resultID); err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		w.WriteHeader(http.StatusOK)
+	}
+}
+
+// UpdateResult updates the result with the given values.
+func UpdateResult(db database.OSBDatabase) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		var result *database.Result
+		// TODO: make result
+		if err := db.UpdateResult(result); err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		w.WriteHeader(http.StatusOK)
+	}
+}
