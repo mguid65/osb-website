31d67422f33fa2293ccebf15e2134966b5ae270e
mwalto7 <dev.mwalto7@gmail.com>
2018-11-16 19:02:55 -0600

    Changed password to flag

--- server/database/database_test.go
+++ server/database/database_test.go
@@ -2,12 +2,8 @@ package database_test
 
 import (
 	"flag"
-	"fmt"
 	"net"
-	"os"
 	"testing"
-
-	"golang.org/x/crypto/ssh/terminal"
 
 	"github.com/mguid65/osb-website/server/database"
 )
@@ -16,19 +12,13 @@ func TestMySQLDB(t *testing.T) {
 	t.Parallel()
 
 	user := flag.String("dbuser", "mwalto7", "the database user")
+	pass := flag.String("dbpass", "", "database password")
 	host := flag.String("dbhost", "127.0.0.1", "the database address")
 	port := flag.String("dbport", "3306", "the database port")
 	name := flag.String("dbname", "osb_test", "the database name")
 	flag.Parse()
 
-	fmt.Fprint(os.Stderr, "DB Password: ")
-	pass, err := terminal.ReadPassword(int(os.Stdin.Fd()))
-	if err != nil {
-		t.Fatalf("could not read password: %v\n", err)
-	}
-	fmt.Fprintln(os.Stderr)
-
-	db, err := database.Connect(*user, string(pass), net.JoinHostPort(*host, *port), *name)
+	db, err := database.Connect(*user, string(*pass), net.JoinHostPort(*host, *port), *name)
 	if err != nil {
 		t.Fatal(err)
 	}
