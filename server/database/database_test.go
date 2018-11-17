0e2415cf8c0a9b7f281f84f2d93a7c04d30171e7
mwalto7 <dev.mwalto7@gmail.com>
2018-11-16 19:06:43 -0600

    fixed flags

--- server/database/database_test.go
+++ server/database/database_test.go
@@ -8,17 +8,28 @@ import (
 	"github.com/mguid65/osb-website/server/database"
 )
 
+var (
+	user string
+	pass string
+	host string
+	port string
+	name string
+)
+
+func init() {
+	flag.StringVar(&user, "dbuser", "mwalto7", "the database user")
+	flag.StringVar(&pass, "dbpass", "", "database password")
+	flag.StringVar(&host, "dbhost", "127.0.0.1", "the database address")
+	flag.StringVar(&port, "dbport", "3306", "the database port")
+	flag.StringVar(&name, "dbname", "osb_test", "the database name")
+}
+
 func TestMySQLDB(t *testing.T) {
 	t.Parallel()
 
-	user := flag.String("dbuser", "mwalto7", "the database user")
-	pass := flag.String("dbpass", "", "database password")
-	host := flag.String("dbhost", "127.0.0.1", "the database address")
-	port := flag.String("dbport", "3306", "the database port")
-	name := flag.String("dbname", "osb_test", "the database name")
 	flag.Parse()
 
-	db, err := database.Connect(*user, string(*pass), net.JoinHostPort(*host, *port), *name)
+	db, err := database.Connect(user, string(pass), net.JoinHostPort(host, port), name)
 	if err != nil {
 		t.Fatal(err)
 	}
