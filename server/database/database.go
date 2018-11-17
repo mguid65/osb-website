73268da725cb1fc9806c8464d90168461a2d5cc8
mwalto7 <dev.mwalto7@gmail.com>
2018-11-16 20:49:02 -0600

    Added required query key

--- server/database/database.go
+++ server/database/database.go
@@ -224,7 +224,7 @@ func (db *mysqlDB) UpdateResult(res *Result) error {
 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
 
-	_, err = updateResult.ExecContext(ctx, res.UserID, res.Scores)
+	_, err = updateResult.ExecContext(ctx, res.UserID, res.Scores, res.ID)
 	return err
 }
 
