30efcb5ab1778f39aaf5bcbf7928b6c59ca7d1ab
mwalto7 <dev.mwalto7@gmail.com>
2018-11-23 13:44:52 -0600

    Fixed test to pass

--- server/database/users_test.go
+++ server/database/users_test.go
@@ -19,36 +19,36 @@ func testUserDB(t *testing.T, db database.UserDatabase) {
 	}
 
 	user.ID = id
-	user.Password = "newsupersecretpassword"
+	user.Name = "updated"
 	if err := db.UpdateUser(user); err != nil {
-		t.Error(err)
+		t.Fatal(err)
 	}
 
 	gotUser, err := db.GetUser(user.ID)
 	if err != nil {
-		t.Error(err)
+		t.Fatal(err)
 	}
 
-	if got, want := gotUser.Password, user.Password; got != want {
+	if got, want := gotUser.Name, user.Name; got != want {
 		t.Errorf("Update user: got %q, want %q", got, want)
 	}
 
-	gotUser, err = db.GetUserByCredentials(user.Name, user.Password)
+	gotUserCred, err := db.GetUserByCredentials(user.Name, user.Password)
 	if err != nil {
-		t.Error(err)
+		t.Fatal(err)
 	}
-	if got, want := gotUser.ID, user.ID; got != want {
+	if got, want := gotUserCred.ID, user.ID; got != want {
 		t.Errorf("Get user by credentials: got %d, want %d", got, want)
 	}
 
 	if err := db.DeleteUser(user.ID); err != nil {
-		t.Error(err)
+		t.Fatal(err)
 	}
 
 	if _, err := db.GetUser(user.ID); err == nil {
-		t.Error("want non-nil error")
+		t.Fatal("want non-nil error")
 	}
 	if _, err := db.GetUserByCredentials(user.Name, user.Password); err == nil {
-		t.Error("want non-nil error")
+		t.Fatal("want non-nil error")
 	}
 }
