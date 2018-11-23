9320c01a7b0272874e663fee5054bb3c1ec425d7
mwalto7 <dev.mwalto7@gmail.com>
2018-11-23 16:25:41 -0600

    Fixed imports

--- src/components/download/Download.jsx
+++ src/components/download/Download.jsx
@@ -1,30 +1,38 @@
-import React, { Component } from 'react';
-import { Paper } from "@material-ui/core";
-import PropTypes from 'prop-types';
-import { withStyles } from '@material-ui/core/styles';
+import React, { Component } from "react";
+import {
+  Paper,
+  Typography,
+  List,
+  ListItem,
+  ListItemIcon,
+  ListItemText,
+  DownloadIcon
+} from "@material-ui/core";
+import PropTypes from "prop-types";
+import { withStyles } from "@material-ui/core/styles";
 
 const styles = theme => ({
   root: {
     maxWidth: "90vw",
     ...theme.mixins.gutters(),
     paddingTop: theme.spacing.unit * 2,
-    paddingBottom: theme.spacing.unit * 2,
+    paddingBottom: theme.spacing.unit * 2
   },
   container: {
-    display: 'flex',
-    flexWrap: 'wrap',
+    display: "flex",
+    flexWrap: "wrap"
   },
   margin: {
-    margin: theme.spacing.unit,
+    margin: theme.spacing.unit
   },
   textField: {
     marginLeft: theme.spacing.unit,
     marginRight: theme.spacing.unit,
-    width: 200,
+    width: 200
   },
   title: {
-    flex: "0 0 auto",
-  },
+    flex: "0 0 auto"
+  }
 });
 
 class Download extends Component {
@@ -32,52 +40,46 @@ class Download extends Component {
     const { classes } = this.props;
 
     return (
-      <Paper className={ classes.root } elevation={1}>
+      <Paper className={classes.root} elevation={1}>
         <Typography gutterBottom variant="h6" component="h2">
           Downloads
         </Typography>
         <Typography gutterBottom variant="subtitle1" component="h2">
           Ubuntu 18.04
         </Typography>
-          <List>
-           <a href="https://opensystembench.com/release/release-1.0.0-Ubuntu-1804.tar"> 
-            <ListItem>        
-             <ListItemIcon>
-              <DownloadIcon />
-             </ListItemIcon>
-             <ListItemText>
-               v1.0.0 .tar
-             </ListItemText>
-            </ListItem>
-           </a>
-           <a href="https://opensystembench.com/release/release-1.0.0-Ubuntu-1804.tar.gz">
+        <List>
+          <a href="https://opensystembench.com/release/release-1.0.0-Ubuntu-1804.tar">
             <ListItem>
-             <ListItemIcon>
-              <DownloadIcon />
-             </ListItemIcon>
-             <ListItemText>
-               v1.0.0 .tar.gz
-             </ListItemText>
+              <ListItemIcon>
+                <DownloadIcon />
+              </ListItemIcon>
+              <ListItemText>v1.0.0 .tar</ListItemText>
             </ListItem>
-           </a>
-           <a href="https://opensystembench.com/release/release-1.0.0-Ubuntu-1804.zip">
+          </a>
+          <a href="https://opensystembench.com/release/release-1.0.0-Ubuntu-1804.tar.gz">
             <ListItem>
-             <ListItemIcon>
-              <DownloadIcon />
-             </ListItemIcon>
-             <ListItemText>
-               v1.0.0 .zip
-             </ListItemText>
+              <ListItemIcon>
+                <DownloadIcon />
+              </ListItemIcon>
+              <ListItemText>v1.0.0 .tar.gz</ListItemText>
             </ListItem>
-           </a>
-          </List>
+          </a>
+          <a href="https://opensystembench.com/release/release-1.0.0-Ubuntu-1804.zip">
+            <ListItem>
+              <ListItemIcon>
+                <DownloadIcon />
+              </ListItemIcon>
+              <ListItemText>v1.0.0 .zip</ListItemText>
+            </ListItem>
+          </a>
+        </List>
       </Paper>
     );
   }
 }
 
 Download.propTypes = {
-  classes: PropTypes.object.isRequired,
+  classes: PropTypes.object.isRequired
 };
 
 export default withStyles(styles)(Download);
